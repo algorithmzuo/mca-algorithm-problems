ac5fd444b61ec0d32f1cacda0e2c2a141f6dca2a
algorithmzuo <algorithmzuo@163.com>
2022-01-22 19:16:32 +0800

    modify code

--- src/mca_04_about_data_structure/Code07_PoemProblem.java
+++ src/mca_04_about_data_structure/Code07_PoemProblem.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 // 给定一个数组arr, 返回最长的有效子序列长度
 // 题目限制 : arr长度 <= 4000, arr中的值<= 10^9
 // 离散化之后，arr长度 <= 4000,  arr中的值<= 4000
-public class Code08_PoemProblem {
+public class Code07_PoemProblem {
 
 	// AABB
 	// ABAB
