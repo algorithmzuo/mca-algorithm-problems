6a47c67893834c89ab0717a1bf80c58584f39b2c
algorithmzuo <algorithmzuo@163.com>
2022-02-21 11:43:30 +0800

    modify code

--- src/test/Code05_MinWindowLength.java
+++ src/test/Code05_MinWindowLength.java
@@ -1,44 +1,6 @@
-package mca_07_about_monotonicity;
+package test;
 
-public class Code07_MinWindowLength {
-
-	// 字符串都是小写字母
-	public static int validMinLen(String s1, String s2) {
-		char[] str1 = s1.toCharArray();
-		char[] str2 = s2.toCharArray();
-		int n = str1.length;
-		int m = str2.length;
-		if (n < m) {
-			return -1;
-		}
-		// n >= m
-		int[] counts = new int[256];
-		for (char s2char : str2) {
-			counts[s2char]++;
-		}
-		int all = m;
-		int ans = Integer.MAX_VALUE;
-		int r = 0;
-		// [i..r)
-		for (int i = 0; i < n; i++) {
-			r = Math.max(r, i);
-			while (r < n && all > 0) {
-				counts[str1[r]]--;
-				if (counts[str1[r]] >= 0) {
-					all--;
-				}
-				r++;
-			}
-			if (all == 0) {
-				ans = Math.min(ans, r - i);
-			}
-			counts[str1[i]]++;
-			if (counts[str1[i]] > 0) {
-				all++;
-			}
-		}
-		return ans == Integer.MAX_VALUE ? -1 : ans;
-	}
+public class Code05_MinWindowLength {
 
 	public static int minLength(String s1, String s2) {
 		if (s1 == null || s2 == null || s1.length() < s2.length()) {
@@ -51,6 +13,9 @@ public class Code07_MinWindowLength {
 			map[str2[i]]++;
 		}
 		int all = str2.length;
+
+		// [L,R-1] R
+		// [L,R) -> [0,0)
 		int L = 0;
 		int R = 0;
 		int minLen = Integer.MAX_VALUE;
@@ -59,10 +24,11 @@ public class Code07_MinWindowLength {
 			if (map[str1[R]] >= 0) {
 				all--;
 			}
-			if (all == 0) {
+			if (all == 0) { // 还完了
 				while (map[str1[L]] < 0) {
 					map[str1[L++]]++;
 				}
+				// [L..R]
 				minLen = Math.min(minLen, R - L + 1);
 				all++;
 				map[str1[L++]]++;
