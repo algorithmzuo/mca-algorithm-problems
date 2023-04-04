5c71cde3ac5f0cb36cd8f16611572ac187afc049
algorithmzuo <algorithmzuo@163.com>
2023-04-04 15:12:29 +0800

    modify code

---
+++ 第03期/class09/Code05_CoinChange.java
@@ -0,0 +1,24 @@
+package class09;
+
+// 测试链接 : https://leetcode.cn/problems/coin-change-ii/
+public class Code05_CoinChange {
+
+	public static int change(int aim, int[] arr) {
+		if (arr == null || arr.length == 0 || aim < 0) {
+			return 0;
+		}
+		int N = arr.length;
+		int[][] dp = new int[N + 1][aim + 1];
+		dp[N][0] = 1;
+		for (int index = N - 1; index >= 0; index--) {
+			for (int rest = 0; rest <= aim; rest++) {
+				dp[index][rest] = dp[index + 1][rest];
+				if (rest - arr[index] >= 0) {
+					dp[index][rest] += dp[index][rest - arr[index]];
+				}
+			}
+		}
+		return dp[0][aim];
+	}
+
+}
